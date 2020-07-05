750a96232c2998e1f2ed2af6225ac8acefd74b64
Jimmy Betesh <jimmybets@gmail.com>
2020-07-05 19:02:40 -0400

    HW53

---
+++ HW53/one.js
@@ -0,0 +1,3 @@
+'Use strict';
+var name = 'Jimmy Betesh';
+var email = 'jimmybets@gmail.com';
---
+++ HW53/two.js
@@ -0,0 +1,5 @@
+'Use strict';
+console.log(name, email);
+
+var aNumber = Number(window.prompt('Please enter your age', ''));
+console.log(aNumber);
