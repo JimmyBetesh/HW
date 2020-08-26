6a6b2cfe67a0108674bb53bb9c31eb5a6c67a677
Jimmy Betesh <jimmybets@gmail.com>
2020-08-25 20:57:54 -0400

    going next branch

--- HW59-FirstJSTest/hw59TestP2.js
+++ HW59-FirstJSTest/hw59TestP2.js
@@ -1,2 +1,7 @@
 // window.app.counter.increment();// this should increment the (variable?) count
 // window.app.counter.getCurrentCount();// print (or return?) the current count value
+let count = 0;
+function increment(num){
+
+}
+increment();
